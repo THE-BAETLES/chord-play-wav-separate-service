fefe1908c52a9e6d88512aea5b8cd986674b850b
chobe111 <chobe1@naver.com>
2022-09-15 14:11:54 +0900

    Mirgrate to Fastapi

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import app, Flask, request,  jsonify
 from services.SeparateService import SeparateService
 from services.VideoService import VideoService
 from dotenv import load_dotenv
 from utils.request import get_youtube_url
+from fastapi import FastAPI
 import os
 
 load_dotenv()
@@ -11,28 +11,20 @@ global model, audio_loader
 input_wav_save_path = os.environ.get("INPUT_WAV_SAVE_PATH")
 output_wav_save_path= os.environ.get("OUTPUT_WAV_SAVE_PATH")
 listen_port = os.environ.get("SERVER_PORT")
-
-app = Flask(__name__)
-
-@app.route('/separate', methods=["GET"])
-def separate() -> str:
-
-    request_params = request.args.to_dict()
-    video_id: str = request_params["videoId"]
-
-    with VideoService(get_youtube_url(video_id),video_id, input_wav_save_path , "mp3") as v:
+app = FastAPI()
+@app.get('/separate')
+def separate(videoId: str) -> str:
+    with VideoService(get_youtube_url(videoId),videoId, input_wav_save_path , "mp3") as v:
         wav_path = v.save_to_volume()
-
-    with SeparateService(wav_path, 16000, video_id, output_path=output_wav_save_path) as s:
+    with SeparateService(wav_path, 16000, videoId, output_path=output_wav_save_path) as s:
         accompaniment_path = s.separate()
 
     response = {
         "accompanimentPath": accompaniment_path,
-        "videoId": video_id
+        "videoId": videoId
     }
-
-    return jsonify(response)
-
+    
+    return response
 def test():
     
     pass
--- utils/request.py
+++ utils/request.py
@@ -5,5 +5,4 @@ def get_request_params(key: str) -> str:
 
 def get_youtube_url(video_id: str) ->str:
     youtube_url = f"https://www.youtube.com/watch?v={video_id}"
-
     return youtube_url
